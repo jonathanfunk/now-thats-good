68f792bed4440651dfd60e3bfecb3d721ba38959
Jonathan Funk <jonfunk88@hotmail.com>
2017-05-31 14:19:34 -0700

    edit account page made

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,3 +38,22 @@ exports.register = async (req, res, next) => {
   await register(user, req.body.password);
   next();
 }
+
+exports.account = (req, res, next) => {
+  res.render('account', { title: 'Edit Your Account' });
+}
+
+exports.updateAccount = async (req, res, next) => {
+  const updates = {
+    name: req.body.name,
+    email: req.body.email
+  }
+
+  const user = await User.findByIdAndUpdate(
+    { _id: req.user._id },
+    { $set: updates },
+    { new: true, runValidators: true, context: 'query' }
+  );
+  req.flash('success', 'Account Updated!');
+  res.redirect('back');
+}
